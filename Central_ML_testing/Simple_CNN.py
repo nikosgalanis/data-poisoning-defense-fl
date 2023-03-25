795a9ac3e1bfbbec02de43a4e9f338b499bdbac6
Nick Galanis <ngalanis99@gmail.com>
2023-03-25 14:49:41 +0000

    Added explainatory graphs for central

--- Central_ML_testing/Simple_CNN.py
+++ Central_ML_testing/Simple_CNN.py
@@ -13,26 +13,36 @@ from tqdm import tqdm
 
 
 class SimpleCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_neurons, drop_rate):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=5)
-        self.fc1 = nn.Linear(3 * 3 * 64, 256)
-        self.fc2 = nn.Linear(256, 10)
+		# net structure: 2 convolutional layers
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+		# a dropout layer
+        self.conv2_drop = nn.Dropout2d(p=drop_rate)
+		# a dense layer
+        self.dense = nn.Linear(320, hidden_neurons)
+        # another dropout layer
+        self.dense_drop = nn.Dropout(p=drop_rate)
+		# an output layer
+        self.fc2 = nn.Linear(hidden_neurons, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        #x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(F.max_pool2d(self.conv3(x), 2))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = x.view(-1, 3 * 3 * 64 )
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        # apply the 2 conv layers
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        # apply the pooling and the droput layers
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        # flatten the output of the convolutional procedure
+        x = x.view(-1, 320)
+        # fully connected layer
+        x = self.dense(x)
+        # the dropout layer
+        x = self.dense_drop(x)
+        # output layer
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        
+        # activation funciton
+        return F.log_softmax(x)
 
 def train_epoch(optimizer, model, trainloader, criterion, device):
     running_loss = 0.0
