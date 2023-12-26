cca43e105e079591543621a70fd0896355892107
Nick Galanis <ngalanis99@gmail.com>
2023-12-26 16:00:27 +0000

    attack experiments complete for mnist cifar

--- DefenseFederated/src/models.py
+++ DefenseFederated/src/models.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+import torch.nn.init as init
 
 
 class CNNMnist(nn.Module):
@@ -79,44 +80,81 @@ class CNNCifar(nn.Module):
     #     # Log softmax for classification
     #     return F.log_softmax(x, dim=1)
 
-### CLASSIC, BUT ENHANCHED BY GPT (works well for lr=0.1, epochs = 50, with many clients (50%) and many epochs per client)
+### CLASSIC, BUT ENHANCHED BY GPT (works well for lr=0.001, epochs = 50
+    # def __init__(self):
+    #     super(CNNCifar, self).__init__()
+
+    #     # Convolutional layers
+    #     self.conv1 = nn.Conv2d(3, 128, 3, padding=1)  # Added padding
+    #     self.bn1 = nn.BatchNorm2d(128)
+    #     self.conv2 = nn.Conv2d(128, 256, 3, padding=1)  # Adjusted kernel size and added padding
+    #     self.bn2 = nn.BatchNorm2d(256)
+    #     self.pool = nn.MaxPool2d(2, 2)
+    #     self.dropout1 = nn.Dropout(0.1)  # Added dropout
+
+    #     # Fully connected layers
+    #     self.fc1 = nn.Linear(256 * 8 * 8, 1024)  # Adjusted dimensions
+    #     self.fc2 = nn.Linear(1024, 512)
+    #     self.dropout2 = nn.Dropout(0.1)  # Added dropout
+    #     self.fc3 = nn.Linear(512, 10)
+
+    # def forward(self, x):
+    #     # Convolutional layers
+    #     x = self.pool(F.relu(self.bn1(self.conv1(x))))
+    #     x = self.pool(F.relu(self.bn2(self.conv2(x))))
+    #     x = self.dropout1(x)
+
+    #     # Flatten the input
+    #     x = x.view(-1, 256 * 8 * 8)  # Adjusted flattening
+
+    #     # Fully connected layers
+    #     x = F.relu(self.fc1(x))
+    #     x = self.dropout2(x)
+    #     x = F.relu(self.fc2(x))
+    #     x = self.fc3(x)
+
+    #     return F.log_softmax(x, dim=1)
+    
+### MOST ENHANCED BY GPT
+
     def __init__(self):
         super(CNNCifar, self).__init__()
 
         # Convolutional layers
-        self.conv1 = nn.Conv2d(3, 128, 3)
+        self.conv1 = nn.Conv2d(3, 128, 3, padding=1)
+        init.kaiming_normal_(self.conv1.weight, mode='fan_out', nonlinearity='leaky_relu')
         self.bn1 = nn.BatchNorm2d(128)
-        self.conv2 = nn.Conv2d(128, 256, 5)
+        self.conv2 = nn.Conv2d(128, 256, 3, padding=1)
+        init.kaiming_normal_(self.conv2.weight, mode='fan_out', nonlinearity='leaky_relu')
         self.bn2 = nn.BatchNorm2d(256)
         self.pool = nn.MaxPool2d(2, 2)
+        self.dropout1 = nn.Dropout(0.1)  # Fine-tuned dropout rate
 
         # Fully connected layers
-        self.fc1 = nn.Linear(256 * 5 * 5, 1024)  # Adjusted dimensions
+        self.fc1 = nn.Linear(256 * 8 * 8, 1024)
+        self.dropout2 = nn.Dropout(0.4)  # Fine-tuned dropout rate
         self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 256)
-        self.fc4 = nn.Linear(256, 128)
-        self.fc5 = nn.Linear(128, 64)
-        self.fc6 = nn.Linear(64, 10)
+        self.dropout3 = nn.Dropout(0.1)  # Fine-tuned dropout rate
+        self.fc3 = nn.Linear(512, 10)
 
     def forward(self, x):
         # Convolutional layers
-        x = self.pool(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.leaky_relu(self.bn1(self.conv1(x)), negative_slope=0.01))
+        x = self.pool(F.leaky_relu(self.bn2(self.conv2(x)), negative_slope=0.01))
+        x = self.dropout1(x)
 
         # Flatten the input
-        x = x.view(-1, 256 * 5 * 5)  # Correctly flatten the tensor
+        x = x.view(-1, 256 * 8 * 8)
 
         # Fully connected layers
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = F.relu(self.fc5(x))
-        x = self.fc6(x)  # Corrected final layer
+        x = F.leaky_relu(self.fc1(x), negative_slope=0.01)
+        x = self.dropout2(x)
+        x = F.leaky_relu(self.fc2(x), negative_slope=0.01)
+        x = self.dropout3(x)
+        x = self.fc3(x)
 
-        # Log softmax for classification
         return F.log_softmax(x, dim=1)
-    
+
 # ### 2ND TIME ENHANCHED BY GPT (works well for lr=0.1, epochs = 50, with many clients (50%) and many epochs per client)
 ## not working well, possibly too much dropout
 #     def __init__(self):
